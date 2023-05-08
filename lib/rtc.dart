1fd784c896adc484a97f5b3033c0aa712091eaa7
song <sos9493@naver.com>
2023-05-08 17:40:15 +0900

    feature : WebRTC 연동
    1. 1:1 WebRTC 연동

--- lib/rtc.dart
+++ lib/rtc.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
@@ -24,6 +26,9 @@ class _RtcState extends State<Rtc> {
   }
 
   Future init() async {
+    await _localRenderer.initialize();
+    await _remoteRenderer.initialize();
+
     await connectSocket();
     await joinRoom();
   }
@@ -36,14 +41,95 @@ class _RtcState extends State<Rtc> {
       print('연결 완료!');
     });
     //
-    socket.on('joined', (data) => {});
-    socket.on('offer', (data) => {});
-    socket.on('answer', (data) => {});
-    socket.on('ice', (data) => {});
+    socket.on('joined', (data) => {_sendOffer()});
+    socket.on('offer', (data) async {
+      data = jsonDecode(data);
+      await _gotOffer(RTCSessionDescription(data['sdp'], data['type']));
+      await _sendAnswer();
+    });
+    socket.on('answer', (data) async {
+      _gotAnswer(RTCSessionDescription(data['sdp'], data['type']));
+    });
+    socket.on('ice', (data) {
+      data = jsonDecode(data);
+      _gotIce(RTCIceCandidate(
+          data['candidate'], data['sdpMid'], data['sdpMLineIndex']));
+    });
   }
 
   Future joinRoom() async {
-    final config = {'iceServers': []};
+    final config = {
+      'iceServers': [
+        {"url": "stun:stun.google.com:19302"},
+      ]
+    };
+
+    final sdpConstraints = {
+      "mandatory": {
+        'offerToReceiveAudio': true,
+        'offerToReceiveVideo': true,
+      },
+      "optional": []
+    };
+
+    pc = await createPeerConnection(config, sdpConstraints);
+
+    final mediaConstraints = {
+      'audio': true,
+      'video': {'facingMode': 'user'}
+    };
+
+    _localStream = await Helper.openCamera(mediaConstraints);
+
+    _localStream!.getTracks().forEach((track) {
+      pc!.addTrack(track, _localStream!);
+    });
+
+    _localRenderer.srcObject = _localStream;
+
+    pc!.onIceCandidate = (ice) {
+      _sendIce(ice);
+    };
+
+    pc!.onAddStream = (stream) {
+      _remoteRenderer.srcObject = stream;
+    };
+
+    socket.emit('join');
+  }
+
+  Future _sendOffer() async {
+    print('send offer');
+    var offer = await pc!.createOffer();
+    pc!.setLocalDescription(offer);
+    socket.emit('offer', JsonEncoder(offer.toMap()));
+  }
+
+  Future _gotOffer(RTCSessionDescription offer) async {
+    print('got offer');
+    pc!.setRemoteDescription(offer);
+  }
+
+  Future _sendAnswer() async {
+    print('send answer');
+    var answer = await pc!.createAnswer();
+    pc!.setLocalDescription(answer);
+    socket.emit('answer', jsonEncode(answer.toMap()));
+  }
+
+  Future _gotAnswer(RTCSessionDescription answer) async {
+    print('got answer');
+    pc!.setLocalDescription(answer);
+  }
+
+  Future _sendIce(RTCIceCandidate ice) async {
+    print('send ice');
+    socket.emit('ice', jsonEncode(ice.toMap()));
+  }
+
+  Future _gotIce(RTCIceCandidate ice) async {
+    print('got ice');
+    pc!.addCandidate(ice);
   }
 
   @override
